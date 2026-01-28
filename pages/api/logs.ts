619adde289d12ee866e6a310a1dfd67927bd5438
drss <drshweta.shah5@gmail.com>
2026-01-28 18:54:32 -0500

    Update logs.ts

--- pages/api/logs.ts
+++ pages/api/logs.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Store } from "@/lib/demostore";
+import { Store } from "@/lib/demoStore";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "GET") {
